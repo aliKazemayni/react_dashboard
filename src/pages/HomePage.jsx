1bd10165d054899aabacd61b3fb86086eba65e7b
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2023-12-06 15:52:31 +0330

    add home and setting page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,11 @@
 import {Helmet} from "react-helmet-async";
-import Grid2 from "@mui/material/Unstable_Grid2";
 import {Typography} from "@mui/material";
-import {Sidebar} from "../components/Sidebar/Sidebar";
 
 export let HomePage = () => {
     return <>
         <Helmet>
-            <title>صفحه اصلی | رزومه من</title>
+            <title>رزومه من | صفحه اصلی</title>
         </Helmet>
-        <Grid2 container>
-            <Sidebar/>
-            <Grid2 sx={{ backgroundColor : "primary." , height : "100vh" }} xs={12} sm={12} md={12-3} lg={12-2} xl={12-2}>
-                <Typography variant="h3">main</Typography>
-            </Grid2>
-        </Grid2>
+        <Typography variant="h3">صفحه اصلی</Typography>
     </>
 }
---
+++ src/pages/SettingPage.jsx
@@ -0,0 +1,11 @@
+import {Helmet} from "react-helmet-async";
+import {Typography} from "@mui/material";
+
+export let SettingPage = () => {
+    return <>
+        <Helmet>
+            <title>رزومه من | تنظیمات</title>
+        </Helmet>
+        <Typography variant="h3">تنظیمات</Typography>
+    </>
+}
