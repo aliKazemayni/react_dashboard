1de5ee1e0b44c96cc2acc58dee06f93abda41c80
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2023-12-16 16:17:48 +0330

    add contrast color in icon list item

--- src/components/Sidebar/SideBarItem.jsx
+++ src/components/Sidebar/SideBarItem.jsx
@@ -13,9 +13,17 @@ export let SideBarItem = ({url , title , icon}) => {
                 marginTop : "5px",
                 '&.active': {
                     backgroundColor: theme.palette.primary.main,
+                    color: theme.palette.primary.contrastText,
+                    '& .MuiListItemIcon-root': {
+                        color: theme.palette.primary.contrastText,
+                    },
                 },
                 '&:hover': {
                     backgroundColor: theme.palette.primary.light,
+                    color: theme.palette.primary.contrastText,
+                    '& .MuiListItemIcon-root': {
+                        color: theme.palette.primary.contrastText,
+                    },
                 },
             }
         )
